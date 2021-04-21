8187cb8990bfa9efd24c27515e7790eb49f5c4f8
Saahil Shangle <saahilshangle@gmail.com>
2021-04-21 11:52:35 -0700

    Created routes to use MongoDB API

--- server/server.js
+++ server/server.js
@@ -11,11 +11,14 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
 	console.log("MongoDB database connection established successfully");
-})
+});
+
+const sampleRouter = require('./routes/sample');
+app.use('/sample', sampleRouter);
 
 app.listen(port, () => {
 	console.log('Server is running on port: ' + port);
