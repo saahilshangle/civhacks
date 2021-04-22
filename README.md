cad29ab02eb047e8efa105c241e8a7fcc0acfadd
Saahil Shangle <saahilshangle@gmail.com>
2021-04-22 13:57:53 -0700

    Front and backend connected

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 #CivHacks 2021
 
-##Setup
-Clone repo, work on separate branch, use pull requests
-
 ##To-Do
-Create parse config process for database access management. Connect front and backend.
+Create parse config process for database access management.
 
 ##Reference Material
-https://www.youtube.com/watch?v=7CqJlxBYj-M @ 55:00
+https://www.youtube.com/watch?v=7CqJlxBYj-M (except using Fetch() API instead of Axios)
--- client/src/views/home.js
+++ client/src/views/home.js
@@ -3,11 +3,63 @@ import './../App.css';
 
 export default class Home extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            usernames: []
+        }
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onSearch = this.onSearch.bind(this);
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const result = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username: this.state.username })
+        };
+        
+        let apiURL = 'http://localhost:5000/sample/add'
+        fetch(apiURL, result)
+            .then(res => res.text())
+            .then(res => console.log(res))
+    }
+
+    onSearch(e) {
+        e.preventDefault();
+        let apiURL = 'http://localhost:5000/sample/'
+        fetch(apiURL)
+            .then(res => res.text())
+            .then(res => this.setState({ usernames: res }))
+    }
+
     render() {
         return (
-            <body className="row">
-                <p>Hello World</p>
-            </body>
+            <div>
+                <h3>Add User to DB</h3>
+                <form onSubmit = {this.onSubmit}>
+                    <label>Username</label>
+                    <input type="text"
+                            required
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}
+                    />
+                    <input type="submit" value="Create User"  />
+                </form>
+                <h3>Existing Users in DB</h3>
+                <button onClick={this.onSearch}>Search</button>
+                <p>{this.state.usernames}</p>
+            </div>
         );
     }
 }
