af66c29b868594e0827ac59312815b66fbca7ca0
Saahil Shangle <saahilshangle@gmail.com>
2021-04-24 02:10:21 -0700

    Backend API GET call with querystring

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Create parse config process for database access management.
 
 ##Reference Material
 https://www.youtube.com/watch?v=7CqJlxBYj-M (except using Fetch() API instead of Axios)
+https://stackabuse.com/the-node-js-request-module/ 
+https://stackoverflow.com/questions/16903476/node-js-http-get-request-with-query-string-parameters
--- server/routes/sample.js
+++ server/routes/sample.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 let User = require('../models/sample.model');
+var request = require('request');
+var url = require('url');
 
 router.route('/').get((req, res) => {
     User.find()
@@ -13,6 +15,25 @@ router.route('/add').post((req, res) => {
     newUser.save()
         .then(() => res.json('User added!'))
         .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/gyms').get((req, res) => {
+    const requestUrl = url.parse(url.format({
+        protocol: 'https',
+        hostname: 'octo-api.asuc.org',
+        pathname: '/gyms/range',
+        query: {
+            latitude: req.body.latitude,
+            longitude: req.body.longitude,
+            radius: req.body.radius,
+            unit: req.body.unit
+        }
+    }));
+    request.get(requestUrl, {
+        'auth': {
+            'bearer': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MTk0NzkwOTgsInR5cGUiOiJhY2Nlc3MiLCJ1aWQiOiJ5UFZNU3RtbFRkYklHQ0hrZWF2cG93VkdMSmcyIn0.3PEwlwCVscRpIelwtoIAgXjInoRulF6JG5ldO2yHHqc'
+        }
+    }).pipe(res)
 });
 
 router.route('/:id').get((req, res) => {
